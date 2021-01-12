c78337237b089665022b7aeaa6e6b02793225a9c
JUNKI555 <junki555@gmail.com>
2021-01-12 23:34:11 +0900

    :construction: test Function

--- Program.vb
+++ Program.vb
@@ -2,6 +2,12 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+        Dim hoge As String = GetHoge()
+        Console.WriteLine("Hello World!:" & hoge)
     End Sub
+
+    Function GetHoge() As String
+        Return "HogeHoge"
+    End Function
+    
 End Module
