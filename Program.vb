06a3a38b3a56afbe6ce20b8725d0f91eb703a45c
JUNKI555 <junki555@gmail.com>
2021-01-13 16:07:27 +0900

    :construction: add sample code

--- Program.vb
+++ Program.vb
@@ -3,11 +3,38 @@ Imports System
 Module Program
     Sub Main(args As String())
         Dim hoge As String = GetHoge()
-        Console.WriteLine("Hello World!:" & hoge)
+        Dim fuga As String = GetFuga(10)
+        Console.WriteLine("Hello World!:" & hoge & ":" & fuga)
     End Sub
 
     Function GetHoge() As String
         Return "HogeHoge"
     End Function
+
+    Function GetFuga(ByVal y As Integer) As String
+        Dim x As Integer = 11
+        Dim fuga As String = "Fuga"
+
+        if x > 10 Then
+            fuga = "Fuga x > 10"
+        End If
+
+        if x < 10 Then
+            fuga = "Fuga x < 10"
+        End If
+
+        if x > 10 Then
+            fuga = "Fuga x > 10"
+        Elseif x = 11
+            fuga = "Fuga x = 10"
+        Else
+            fuga = "Fuga x < 10"
+        End If
+
+        Console.WriteLine(fuga)
+
+        Return fuga
+    End Function
     
+
 End Module
